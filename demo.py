89234dd233d5d303a1842ff0f8ad27de41924feb
luongtruong20201 <89332876+luongtruong20201@users.noreply.github.com>
2022-10-19 21:38:22 +0700

    ^^

---
+++ demo.py
@@ -0,0 +1,36 @@
+from pyexpat import model
+import pandas as pd
+import numpy as np
+from sklearn import model_selection, metrics, linear_model, tree
+
+data = pd.read_csv('./data.csv', sep=';').to_numpy()
+
+train, test = model_selection.train_test_split(
+    data,
+    test_size=0.3,
+    random_state=None
+)
+
+X_train, y_train, X_test, y_test = train[:, :-
+                                         1], train[:, -1:], test[:, :-1], test[:, -1:]
+
+pla = linear_model.Perceptron()
+pla.fit(X_train, y_train.ravel())
+y_pred = pla.predict(X_test)
+print(pla.score(X_test, y_test))
+
+#CART
+
+cart = tree.DecisionTreeClassifier(criterion='gini')
+cart.fit(X_train, y_train)
+
+y_pred = cart.predict(X_test)
+
+print(cart.score(X_test, y_test))
+
+# ID3
+id3 = tree.DecisionTreeClassifier(criterion='entropy')
+id3.fit(X_train, y_train)
+
+y_pred = id3.predict(X_test)
+print(id3.score(X_test, y_test))
